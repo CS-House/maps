ef2a3d3c2cf49cf6113e18e8e6d05bc010d128b7
gowtham-munukutla <gowtham.m81197@gmail.com>
2018-06-26 18:56:55 +0530

    completed backend. The script.go generates random input for the tcp

---
+++ script.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"math/rand"
+	"net"
+	"os/exec"
+	"strconv"
+	"time"
+)
+
+func main() {
+	dids := [10]string{"EUgnN1N1Eu", "MRl06xqlYR", "R6E2YeGHi5",
+		"G9ceDX6fTT", "Zwkpbm93vj", "gFGKPNdTiO",
+		"Py5qQAcf0i", "GlwuEiwrHO", "9WMPR3n0J9", "QkypngR4p4"}
+
+	lats := [10]float64{85.23645, 23.45668, 36.12485, 89.45218, 62.31548, 49.56321, 78.13698, 49.65217, 36.91254, 69.13547}
+
+	longs := [10]float64{62.15477, 76.31485, 53.12968, 79.13654, 49.36574, 43.85412, 19.21547, 63.28745, 37.26845, 95.63247}
+
+	ln, err := net.Dial("tcp", "192.168.1.3:8000")
+	if err != nil {
+		log.Fatalln("connection messed up", err.Error())
+		panic(err)
+	}
+	defer ln.Close()
+
+	for {
+		var buffer bytes.Buffer
+
+		str, integer := random(1, 9)
+		buffer.WriteString("GTPL $" + str + "," + dids[integer] + ",A,")
+
+		out, _ := exec.Command("/bin/bash", "-c", "date +%d%m%y,%I%M%S | tr -d '\n'").Output()
+
+		lats[integer] = lats[integer] + 0.0015
+		latstr := FloatToString(lats[integer])
+
+		buffer.WriteString(string(out) + "," + latstr + ",N,")
+
+		longs[integer] = longs[integer] + 0.0015
+		longstr := FloatToString(longs[integer])
+		buffer.WriteString(longstr + ",E,0,406,309,11,0,14,1,0,26.4470#")
+
+		fmt.Println(buffer.String())
+
+		m, err := ln.Write(buffer.Bytes())
+		if err != nil {
+			log.Fatalln("write messed up", err.Error())
+			panic(err)
+		}
+		fmt.Println("Wrote ", strconv.Itoa(m)+", bytes")
+
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func random(min, max int) (string, int) {
+	rand.Seed(time.Now().Unix())
+	integer := rand.Intn(max-min) + min
+
+	return strconv.Itoa(integer), integer
+}
+
+func FloatToString(input_num float64) string {
+	// to convert a float number to a string
+	return strconv.FormatFloat(input_num, 'f', 5, 32)
+}
+
+// GTPL $1,867322035135813,A,290518,062804,18.709738,N,80.068397,E,0,406,309,11,0,14,1,0,26.4470#

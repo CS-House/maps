58299f5b344968efd24f1048bc3850a43ae97653
gowtham-munukutla <gowtham.m81197@gmail.com>
2018-06-30 01:00:35 +0530

    working gmaps without animation

--- broker.go
+++ broker.go
@@ -9,10 +9,18 @@ var db *sql.DB
 func main() {
 	db, err := sql.Open("mysql", "rootone:Test!@#$@tcp(139.59.90.102:3306)/kudankalam")
 
+	check(err)
+
 }
 
 func check(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+type JsonObj struct {
+	deviceID string
+	lat      string
+	long     string
 }
